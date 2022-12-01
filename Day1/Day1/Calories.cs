e150bad0197d7a29dab979345521559779ea0046
James Longstaffe <james.longstaffe@red-gate.com>
2022-12-01 11:39:48 +0000

    Day1

---
+++ Day1/Day1/Calories.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Day1;
+
+public static class Calories
+{
+    public static int MaxCalories(string input)
+    {
+        return GetGroups(input).Max(x => x.Sum());
+    }
+
+    public static int TopThreeCalories(string input)
+    {
+        return GetGroups(input).Select(c => c.Sum())
+                               .OrderByDescending(x => x)
+                               .Take(3)
+                               .Sum();
+    }
+
+    private static IEnumerable<IEnumerable<int>> GetGroups(string input)
+    {
+        return input.Split("\n\n")
+                    .Select(c => c.Split('\n').Select(int.Parse));
+    }
+}
