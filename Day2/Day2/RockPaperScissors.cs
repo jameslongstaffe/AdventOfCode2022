44660d09934d75f5a0c7f8f382d6af6541c01433
James Longstaffe <james.longstaffe@red-gate.com>
2022-12-07 13:53:30 +0000

    Day 2

---
+++ Day2/Day2/RockPaperScissors.cs
@@ -0,0 +1,84 @@
+﻿
+#pragma warning disable CS8524
+#pragma warning disable CS8846
+
+using static Day2.RockPaperScissors.Hand;
+using static Day2.RockPaperScissors.Result;
+
+namespace Day2;
+public static class RockPaperScissors
+{
+    public static int Part1_GetScore()
+    {
+        return GetHands().Sum(h => ((int) GetMatchResult(h)) * 3
+                                 + ((int) h.player) + 1);
+    }
+
+    public static int Part2_GetScore()
+    {
+        return GetHandAndResults()
+              .Sum(h => ((int) h.result * 3)
+                      + ((int) GetHandForResult(h.opponent, h.result)) + 1);
+    }
+
+    private static Result GetMatchResult((Hand opponent, Hand player) hands)
+    {
+        return hands switch
+        {
+            var (opponent, player) when opponent == player => Draw,
+
+            (Rock, Paper)     => Win,
+            (Rock, Scissors)  => Lose,
+
+            (Paper, Rock)     => Lose,
+            (Paper, Scissors) => Win,
+
+            (Scissors, Rock)  => Win,
+            (Scissors, Paper) => Lose
+        };
+    }
+
+    private static Hand GetHandForResult(Hand opponent, Result result)
+    {
+
+        return opponent switch
+        {
+            Rock => result switch
+            {
+                Lose => Scissors,
+                Draw => Rock,
+                Win => Paper
+            },
+
+            Paper => result switch
+            {
+                Lose => Rock,
+                Draw => Paper,
+                Win => Scissors
+            },
+
+            Scissors => result switch
+            {
+                Lose => Paper,
+                Draw => Scissors,
+                Win => Rock
+            }
+        };
+    }
+
+    private static IEnumerable<(Hand opponent, Hand player)> GetHands()
+    {
+        return File.ReadAllLines("input.txt")
+                   .Select(l => ((Hand) (l[0] - 'A'), (Hand) (l[2] - 'X')));
+    }
+
+    private static IEnumerable<(Hand opponent, Result result)> GetHandAndResults()
+    {
+        return File.ReadAllLines("input.txt")
+                   .Select(l => ((Hand) (l[0] - 'A'), (Result) (l[2] - 'X')));
+    }
+
+    public enum Hand { Rock, Paper, Scissors }
+
+    public enum Result { Lose, Draw, Win }
+}
---
+++ Day2/Day2/RockPaperScissorsTests.cs
@@ -0,0 +1,19 @@
+﻿
+using NUnit.Framework;
+
+namespace Day2;
+
+internal class RockPaperScissorsTests
+{
+    [Test]
+    public void Test_Part1()
+    {
+        Assert.That(RockPaperScissors.Part1_GetScore(), Is.EqualTo(9241));
+    }
+
+    [Test]
+    public void Test_Part2()
+    {
+        Assert.That(RockPaperScissors.Part2_GetScore(), Is.EqualTo(14610));
+    }
+}
