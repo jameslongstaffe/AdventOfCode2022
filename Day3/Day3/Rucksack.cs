984b0ef3bc7959cd41de3c426c716c5916577ac9
James Longstaffe <james.longstaffe@red-gate.com>
2022-12-07 14:40:58 +0000

    Day 3

---
+++ Day3/Day3/Rucksack.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Day3;
+
+public static class Rucksack
+{
+    public static int Part1_GetSum()
+    {
+        static char GetWrongItem(string s) =>
+            s[..(s.Length / 2)].Intersect(s[(s.Length / 2)..]).Single();
+
+        return File.ReadAllLines("input.txt")
+                   .Select(GetWrongItem)
+                   .Sum(GetPriority);
+    }
+
+    public static int Part2_GetSum()
+    {
+        static char GetCommonItem(string[] s)
+        {
+            return s[1..].Aggregate(s[0].ToCharArray().AsEnumerable(),
+                                    (result, x) => result.Intersect(x.ToCharArray()))
+                         .Single();
+        }
+
+        return File.ReadAllLines("input.txt")
+                   .Chunk(3)
+                   .Select(GetCommonItem)
+                   .Sum(GetPriority);
+    }
+
+    private static int GetPriority(char c) =>
+        char.IsLower(c) ? c - 'a' + 1 : c - 'A' + 27;
+}
---
+++ Day3/Day3/RucksackTests.cs
@@ -0,0 +1,19 @@
+﻿
+using NUnit.Framework;
+
+namespace Day3;
+
+internal class RucksackTests
+{
+    [Test]
+    public void Test_Part1()
+    {
+        Assert.That(Rucksack.Part1_GetSum(), Is.EqualTo(8298));
+    }
+
+    [Test]
+    public void Test_Part2()
+    {
+        Assert.That(Rucksack.Part2_GetSum(), Is.EqualTo(2708));
+    }
+}
