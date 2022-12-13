69a5617bb3dbd3671dc92bd34b96a18b7e683095
James Longstaffe <james.longstaffe@red-gate.com>
2022-12-13 15:44:28 +0000

    Day 4

---
+++ Day4/Day4/Rucksack.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Day4;
+
+public static class Rucksack
+{
+    public static int Part1()
+    {
+        static bool FullyContains(int[] ranges)
+        {
+            return (ranges[0] >= ranges[2] && ranges[1] <= ranges[3])
+                || (ranges[2] >= ranges[0] && ranges[3] <= ranges[1]);
+        }
+
+        return GetInputArray().Count(FullyContains);
+    }
+
+    public static int Part2()
+    {
+        static bool Overlaps(int[] ranges)
+        {
+            return (ranges[2] <= ranges[1] && ranges[2] >= ranges[0])
+                || (ranges[0] >= ranges[2] && ranges[0] <= ranges[3]);
+        }
+
+        return GetInputArray().Count(Overlaps);
+    }
+
+    private static IEnumerable<int[]> GetInputArray()
+    {
+        return File.ReadAllLines("input.txt")
+                   .Select(l => l.Split('-', ',').Select(int.Parse).ToArray());
+    }
+}
---
+++ Day4/Day4/RucksackTests.cs
@@ -0,0 +1,19 @@
+﻿
+using NUnit.Framework;
+
+namespace Day4;
+
+internal class RucksackTests
+{
+    [Test]
+    public void Test_Part1()
+    {
+        Assert.That(Rucksack.Part1(), Is.EqualTo(571));
+    }
+
+    [Test]
+    public void Test_Part2()
+    {
+        Assert.That(Rucksack.Part2(), Is.EqualTo(917));
+    }
+}
